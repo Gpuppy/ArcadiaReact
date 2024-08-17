a9ed52846c38438cf9b5ee3cda3f86259fe49dea
Gael Adam <gaelswim@hotmail.com>
2024-08-17 11:33:58 +0200

    make it better

--- src/components/Animals.js
+++ src/components/Animals.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from "react";
 function Animals()
 {
     const [useData, setAnimalData] = useState([]);
+
     return(
         <React.Fragment>
             <h5>Animals</h5>
